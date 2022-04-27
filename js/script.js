110dedcac6fdbf22997454359b334862ecc61320
Bodya <jokerlinnik@gmail.com>
2022-04-27 13:53:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () =>{
+    menu.classList.add('active'); 
+});
+closeElem.addEventListener('click', () =>{
+    menu.classList.remove('active'); 
+});
+
+const counters= document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) =>{
+    lines[i].style.width = item.innerHTML;
+}); 
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 800){
+        $('.pageup').fadeIn();
+    } else{
+      $('.pageup').fadeOut();
+    }
+  });
